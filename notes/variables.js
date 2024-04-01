cd756e586f2dfbb17b20434767ff7bc0755312d2
Shivanshu Maurya <143547752+shivanshumaurya11@users.noreply.github.com>
2024-04-01 19:28:51 +0000

    done

---
+++ notes/variables.js
@@ -0,0 +1,17 @@
+const rollNo = 832
+let accountEmail = "ajlkj@gmail.xom"
+var accountPassword = "290438"
+accountCity = "Jaipur"
+let acc;
+// rollNo = 32 //not allowed
+
+accountEmail = "jka@jafkljl.cpo"
+accountPassword = "23908"
+accountCity = "Delhi"
+
+
+console.log(rollNo)
+console.table([accountEmail,accountPassword,accountCity,rollNo,acc])
+/*Prefer Not to use var Use let
+   Because of issue in block scope and functional scope
+*/
