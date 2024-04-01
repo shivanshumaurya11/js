59e647171ff3d88d33e3c350404018edb70c1b31
Shivanshu Maurya <143547752+shivanshumaurya11@users.noreply.github.com>
2024-04-01 19:48:52 +0000

    done

---
+++ notes/datatype.js
@@ -0,0 +1,21 @@
+"use strict"; //treat all js code as newer version
+
+// alert("hello") // we're using nodejs not browser
+
+let name = "Shivanshu"
+let rollno = 21
+let male = true
+
+//number => 2 to power 53
+// bigint
+// String => ""
+// boolean => true/false
+// null =>stand alone value(value nhi hai ya empty hai)
+// undefined => value abhi assign nhi hui
+// symbol => uniqueness
+
+// object 
+
+console.log(typeof undefined);//undefined
+console.log(typeof null);//object
+
